f47934d88cda7e94d5dd337732ece7d4b6c825d0
DevanaFattah <dafrenov@gmail.com>
2022-10-02 14:41:15 +0700

    feat: added cover image border radius

--- lib/widgets/cover_image_widget.dart
+++ lib/widgets/cover_image_widget.dart
@@ -5,14 +5,17 @@ class CoverImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Container(
-        color: Colors.blue.shade300,
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+          color: Colors.blue.shade300,
+          borderRadius: BorderRadius.all(
+            Radius.circular(35),
+          ),
+        ),
         child: Stack(
           children: <Widget>[
             Image.asset('assets/images/beach.jpg',
-                width: double.infinity, 
-                height: coverHeight, 
-                fit: BoxFit.cover
-              ),
+                width: double.infinity, height: coverHeight, fit: BoxFit.cover),
           ],
         ),
       );
